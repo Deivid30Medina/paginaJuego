384e9a81f6f811c85fe37dab40805f9015910714
Deivid30Medina <deivis32sebas02@gmail.com>
2023-11-17 23:15:30 -0500

    Agregando pantalla completa

---
+++ js/pantallaCompleta.js
@@ -0,0 +1,13 @@
+document.getElementById('fullscreenButton').addEventListener('click', function() {
+    var elem = document.documentElement;
+
+    if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+    } else if (elem.mozRequestFullScreen) { /* Firefox */
+      elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari y Opera */
+      elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE/Edge */
+      elem.msRequestFullscreen();
+    }
+  });
