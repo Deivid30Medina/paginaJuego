d3fbfc06f481c0e1f5f11076573d96b7e96c2db8
Deivid30Medina <deivis32sebas02@gmail.com>
2023-11-18 00:12:01 -0500

    Agregando pantalla completa

--- js/pantallaCompleta.js
+++ js/pantallaCompleta.js
@@ -1,13 +1,15 @@
-document.getElementById('fullscreenButton').addEventListener('click', function() {
-    var elem = document.documentElement;
+document.getElementById('fullscreenButton').addEventListener('click', function(event) {
+  event.preventDefault(); // Evita que el formulario se envíe
 
-    if (elem.requestFullscreen) {
-      elem.requestFullscreen();
-    } else if (elem.mozRequestFullScreen) { /* Firefox */
-      elem.mozRequestFullScreen();
-    } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari y Opera */
-      elem.webkitRequestFullscreen();
-    } else if (elem.msRequestFullscreen) { /* IE/Edge */
-      elem.msRequestFullscreen();
-    }
-  });
+  var elem = document.documentElement;
+
+  if (elem.requestFullscreen) {
+    elem.requestFullscreen();
+  } else if (elem.mozRequestFullScreen) { /* Firefox */
+    elem.mozRequestFullScreen();
+  } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari y Opera */
+    elem.webkitRequestFullscreen();
+  } else if (elem.msRequestFullscreen) { /* IE/Edge */
+    elem.msRequestFullscreen();
+  }
+});
