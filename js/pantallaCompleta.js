9f83fd08b53be1968337f702b43a56449680c3ed
Deivid30Medina <deivis32sebas02@gmail.com>
2023-11-18 22:25:50 -0500

    Probando bloque horizontal

--- js/pantallaCompleta.js
+++ js/pantallaCompleta.js
@@ -1,6 +1,37 @@
-document.getElementById('fullscreenButton').addEventListener('click', function(event) {
+document.addEventListener('click', function(event) {
   event.preventDefault(); // Evita que el formulario se envíe
+  requestFullScreen();
+});
 
+document.addEventListener('DOMContentLoaded', function() {
+  // Verificar el ancho de la pantalla al cargar la página
+  checkScreenWidth();
+
+  // Agregar un listener para el evento de cambio de tamaño de la pantalla
+  window.addEventListener('resize', checkScreenWidth);
+
+  
+});
+
+function checkScreenWidth() {
+  var screenWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
+
+  // Si el ancho de la pantalla es menor que 800px, aplicar bloqueo y pantalla completa
+  if (screenWidth < 800) {
+    lockOrientation();
+  }
+}
+
+function lockOrientation() {
+  // Bloquear la orientación en landscape
+  if (screen.orientation && screen.orientation.lock) {
+    screen.orientation.lock('landscape');
+  } else if (screen.lockOrientation) {
+    screen.lockOrientation('landscape');
+  } // Puedes agregar más casos según sea necesario para otros navegadores
+}
+
+function requestFullScreen() {
   var elem = document.documentElement;
 
   if (elem.requestFullscreen) {
@@ -12,4 +43,5 @@ document.getElementById('fullscreenButton').addEventListener('click', function(e
   } else if (elem.msRequestFullscreen) { /* IE/Edge */
     elem.msRequestFullscreen();
   }
-});
+}
+
