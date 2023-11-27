a26fd27e8e3e33df145b879ef6ad9902121d690e
Deivid30Medina <144353360+Deivid30Medina@users.noreply.github.com>
2023-11-27 15:33:25 -0500

    Se ajsuto el pausar video

--- js/sonidoExplicacion.js
+++ js/sonidoExplicacion.js
@@ -1,56 +1,66 @@
+var reproduciendo = true;
+
 document.addEventListener("DOMContentLoaded", function () {
   // Obtén el elemento de audio
   var miAudio = document.getElementById("idAudiopExplicacion");
-
-  // Inicializa la reproducción del audio después de una interacción del usuario
-  document.addEventListener(
-    "click",
-    function iniciarReproduccionDespuesDeClic() {
-      // Elimina el listener para no volver a reproducir después de la interacción inicial
-      document.removeEventListener("click", iniciarReproduccionDespuesDeClic);
-
-      // Agrega un evento para pausar la reproducción después de la primera reproducción
-      miAudio.addEventListener("ended", function () {
-        pausarYReiniciar(miAudio);
-      });
-
-      // Reproduce el audio
-      miAudio.play();
-    }
-  );
-
   validarBtnSonido(miAudio);
-  
 });
 
-function cargarDocumentoSVG(idSvg) {
-    return new Promise((resolve, reject) => {
-        let svgBtn = document.getElementById(idSvg);
-        window.addEventListener('load', function () {
-            let documentSvg = svgBtn.contentDocument;
-            if (documentSvg) {
-                resolve(documentSvg);
-            } else {
-                reject(new Error('Error al cargar el documento SVG.'));
-            }
-        });
-    });
+function validarPausarAudio(miAudio){
+  // Reproduce el audio si no está actualmente reproduciéndose
+  if (!reproduciendo) {
+    miAudio.play();
+    reproduciendo = true;
+  } else {
+    // Si ya está reproduciéndose, pausa y reinicia al principio
+    pausarYReiniciar(miAudio);
+    reproduciendo = false;
+  }
 }
 
-async function validarBtnSonido(miAudio){
-    try {
-        let documentSvg = await cargarDocumentoSVG('idSvgMusica');
-        // Selecciona todos los elementos con la clase '.classsSonido' dentro del documento SVG
-        let elementsSonido = documentSvg.querySelectorAll(".classsSonido");
-        // Para cada elemento con la clase '.classsSonido', agrega un evento de clic
-        elementsSonido.forEach(function (elementoSVG) {
-            elementoSVG.onclick = function () {
-                reiniciarReproduccion(miAudio);
-            };
-        });
-    } catch (error) {
-        console.error(error.message);
-    }
+function iniciarReproduccionDespuesDeClic2(miAudio) {
+  // Agrega un evento para pausar la reproducción después de la primera reproducción
+  miAudio.addEventListener("ended", function () {
+    pausarYReiniciar(miAudio);
+    reproduciendo = false;
+  });
+  validarPausarAudio(miAudio);
+  
+}
+
+function cargarDocumentoSVG(idSvg) {
+  return new Promise((resolve, reject) => {
+    let svgBtn = document.getElementById(idSvg);
+    window.addEventListener("load", function () {
+      let documentSvg = svgBtn.contentDocument;
+      if (documentSvg) {
+        resolve(documentSvg);
+      } else {
+        reject(new Error("Error al cargar el documento SVG."));
+      }
+    });
+  });
+}
+
+async function validarBtnSonido(miAudio) {
+  try {
+    let documentSvg = await cargarDocumentoSVG("idSvgMusica");
+    // Selecciona todos los elementos con la clase '.classsSonido' dentro del documento SVG
+    let elementsSonido = documentSvg.querySelectorAll(".classsSonido");
+    // Para cada elemento con la clase '.classsSonido', agrega un evento de clic
+    elementsSonido.forEach(function (elementoSVG) {
+      elementoSVG.onclick = function () {
+        if(reproduciendo){
+          validarPausarAudio(miAudio);
+        }else{
+          iniciarReproduccionDespuesDeClic2(miAudio);
+        }
+        
+      };
+    });
+  } catch (error) {
+    console.error(error.message);
+  }
 }
 
 function pausarYReiniciar(audioElement) {
