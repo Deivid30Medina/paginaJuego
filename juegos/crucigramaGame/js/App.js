032c18d8e3e724a462bfd5338da581198cb05a18
Deivid30Medina <144353360+Deivid30Medina@users.noreply.github.com>
2023-12-01 14:24:12 -0500

    Ajustando juego4

--- juegos/crucigramaGame/js/App.js
+++ juegos/crucigramaGame/js/App.js
@@ -1,4 +1,16 @@
 //Hola
 import { validarTexto } from "../js/validarPalabra.js";
+import {startTemporizador} from '../../../js/temporizador.js'
 
+let label = document.querySelector('label[for="Labeltiempo"]');
+// Captura el valor del contenido del label
+let tiempo = label.innerText || label.textContent;
+
+let labelPerdio = document.querySelector('label[for="LabelPaginaPerdio"]');
+// Captura el valor del contenido del label
+let paginaPerdio = labelPerdio.innerText || label.textContent;
 validarTexto();
+setTimeout(function() {
+    startTemporizador(tiempo, paginaPerdio);
+}, 2000);
+
