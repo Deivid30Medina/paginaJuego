f80e1b7b5f7e8ff4fa2e2a1f5e3ebd03fc34823c
Deivid30Medina <144353360+Deivid30Medina@users.noreply.github.com>
2023-12-04 14:13:50 -0500

    Ajsutando estructura

---
+++ js/agregarTiempoTemporizador.js
@@ -0,0 +1,13 @@
+import {startTemporizador} from './temporizador.js'
+let label = document.querySelector('label[for="Labeltiempo"]');
+// Captura el valor del contenido del label
+let tiempo = label.innerText || label.textContent;
+
+let labelPerdio = document.querySelector('label[for="LabelPaginaPerdio"]');
+// Captura el valor del contenido del label
+let paginaPerdio = labelPerdio.innerText || label.textContent;
+setTimeout(function() {
+    startTemporizador(tiempo, paginaPerdio);
+}, 2000);
+
+ 
